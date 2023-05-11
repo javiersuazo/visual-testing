edf33ba4b25fdeb859c1514cfcea4f4d03fc80b5
Javier <javier_suazo@live.com>
2023-05-11 16:18:06 +0200

    test

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -4,7 +4,7 @@ import './button.css'; // Import the CSS file for the button
 export const Button = (props) => {
   return (
     <button className="my-button" onClick={props.onClick}>
-      PRESS ME
+      PRESS ME 1
     </button>
   );
 }
